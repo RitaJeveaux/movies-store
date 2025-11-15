e762ab50fbe4f38503859063b16b598b981cf403
RitaJeveaux <ritajeveaux@gmail.com>
2025-11-15 08:48:47 -0300

    criado not-found

--- src/app/pages/movie-list/movie-list.ts
+++ src/app/pages/movie-list/movie-list.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movie } from '../../models/movie';
+import { MovieCard } from '../../components/movie-card/movie-card';
+import { MovieService } from '../../services/movie-service';
 
 @Component({
   selector: 'app-movie-list',
-  imports: [],
+  imports: [MovieCard],
   templateUrl: './movie-list.html',
   styleUrl: './movie-list.css',
 })
-export class MovieList {
+export class MovieList implements OnInit {
+  movies: Array<Movie> = [];
 
+  constructor(private movieService: MovieService) { }
+
+  ngOnInit() {
+    this.movieService.getAll().subscribe((movies: Array<Movie>) => {
+      console.log(movies);
+      this.movies = movies;
+
+    });
+  }
+  deleteCard(id: string) {
+    this.movies = this.movies.filter((movie) => movie.id !== id);
+  }
 }
