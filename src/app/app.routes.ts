5b61f5a8e3807ff93537329b1206c34baa348fd5
RitaJeveaux <ritajeveaux@gmail.com>
2025-11-15 10:13:49 -0300

    header atualizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { MovieAdd } from './pages/movie-add/movie-add';
+import { MovieList } from './pages/movie-list/movie-list';
+import { Login } from './pages/login/login';
+import { NotFound } from './pages/not-found/not-found';
+import { authGuard } from './guards/auth-guard';
+import { MovieEdit } from './pages/movie-edit/movie-edit';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'movies', pathMatch: 'full' },
+  { path: 'movies', component: MovieList },
+  { path: 'add', component: MovieAdd, canActivate: [authGuard] },
+  { path: 'edit/:id', component: MovieEdit },
+  { path: 'login', component: Login },
+  { path: '**', component: NotFound },
+];
+
+
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -2,13 +2,23 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  constructor(private authService: AuthService) { }
 
+  get isLoggedIn(): boolean {
+    return this.authService.isAuthenticated();
+  }
+
+  logoutCurrentUser() {
+    this.authService.logout();
+  }
 }
