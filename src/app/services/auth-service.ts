ea6980113b376e6fd75278668cb0c05cd469d5a9
RitaJeveaux <ritajeveaux@gmail.com>
2025-11-20 18:33:23 -0300

    autenticação do token funcionando

--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -1,25 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
 import { map, Observable, tap } from 'rxjs';
 
-type LoginResponse =  { token: string };
+type LoginResponse = { token: string };
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   API_URL: string = "http://localhost:3000/";
+  private expTimer: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   isAuthenticated() {
     const loggedUser = JSON.parse(localStorage.getItem("loggedUser") || "null");
     return loggedUser != null;
   }
 
+  isTokenExpired(): boolean {
+    const token = this.getToken();
+    if (!token) return false;
+
+    const payload = jwtDecode(token);
+    if (!payload.exp) return false;
+
+    const now = Math.floor(Date.now() / 1000);
+    return payload.exp <= now;
+  }
+
   getToken(): string | null {
-    const loggedUser = JSON.parse(localStorage.getItem("loggedUser") || "null");
-    return loggedUser?.token || null;
+    const { token } = JSON.parse(localStorage.getItem('loggedUser') || '{}');
+    if (!token) return null;
+    return token;
   }
 
   loginUser(email: string, password: string): Observable<boolean> {
@@ -27,12 +41,36 @@ export class AuthService {
       .pipe(
         tap(({ token }: LoginResponse) => {
           localStorage.setItem("loggedUser", JSON.stringify({ token }));
+          this.scheduleLogout();
         }),
-        map(res => res?.token? true : false)
+        map(res => res?.token ? true : false)
       );
   }
 
   logout() {
     localStorage.removeItem("loggedUser");
+  }
+  scheduleLogout() {
+    clearTimeout(this.expTimer);
+
+    const token = this.getToken();
+    if (!token) return;
+
+    const payload = jwtDecode(token);
+    if (!payload.exp) return;
+
+    const now = Math.floor(Date.now() / 1000);
+    const timeUntilExp = Math.max((payload.exp - now), 0);
+
+    this.expTimer = setTimeout(() => this.logout(), timeUntilExp * 1000);
+  }
+
+  checkLoginExpiration() {
+    if (this.isTokenExpired()) {
+      this.logout();
+      return;
+    }
+
+    this.scheduleLogout();
   }
 }
